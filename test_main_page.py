7a8c5b503e71add2e919c859f166fdebea2c28f2
Flash <flashikk@inbox.ru>
2021-09-03 03:32:58 +0500

    add locators

--- test_main_page.py
+++ test_main_page.py
@@ -2,10 +2,6 @@ from .pages.main_page import MainPage
 
 link = "http://selenium1py.pythonanywhere.com/"
 
-def goto_login_page(browser):
-    login_link = browser.find_element_by_css_selector("#login_link")
-    login_link.click()
-
 def test_guest_can_goto_login_page(browser):
     page = MainPage(browser, link)
     page.open()
